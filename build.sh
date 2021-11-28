65a90dec226986d5c4328811e9bedce5de848086
Greg Frazier <glfrazier@alum.mit.edu>
2021-11-28 18:00:34 -0500

    Unifying the Linux and Cygwin build processes.

--- build.sh
+++ build.sh
@@ -1,5 +1,16 @@
 #!/usr/bin/env bash 
 set -x -e
-mvn package -DskipTests
-mvn install:install-file -Dfile=target/StateMachine-0.0.1.jar -DgroupId=com.github.glfrazier -DartifactId=StateMachine -Dversion=0.0.1 -Dpackaging=jar -DlocalRepositoryPath=/home/pi/mvn-repo
+KERNEL=`uname -s`
+ID=StateMachine
+VER=0.0.1
+
+if [[  $KERNEL == "Linux" ]]; then
+  mvn package -DskipTests
+  REPO=/home/pi/mvn-repo
+else
+  mvn -f pom.cyg.xml package -DskipTests
+  REPO=../mvn-repo
+fi
+
+mvn install:install-file -Dfile=target/${ID}-${VER}.jar -DgroupId=com.github.glfrazier -DartifactId=${ID} -Dversion=${VER} -Dpackaging=jar -DlocalRepositoryPath=${REPO}
 
